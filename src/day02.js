0a628086318566551fcbf62808a39536fd781ef9
Christian Kalinowski <christian.kalinowski@covestro.com>
2022-12-02 09:29:20 +0100

    Added Day 02 solution

---
+++ src/day02.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const scores = {
+  "A X": 1 + 3,
+  "A Y": 2 + 6,
+  "A Z": 3,
+  "B X": 1,
+  "B Y": 2 + 3,
+  "B Z": 3 + 6,
+  "C X": 1 + 6,
+  "C Y": 2,
+  "C Z": 3 + 3,
+}
+
+const scores2 = {
+  "A X": 3,
+  "A Y": 3 + 1,
+  "A Z": 6 + 2,
+  "B X": 1,
+  "B Y": 3 + 2,
+  "B Z": 6 + 3,
+  "C X": 2,
+  "C Y": 3 + 3,
+  "C Z": 6 + 1,
+}
+
+function day02() {
+  const data = fs.readFileSync("../resources/day02.input.txt", "utf-8").trim();
+  const rows = data.split("\r\n")
+  console.log(rows.map(row => scores[row])
+    .reduce((a, b) => a + b, 0));
+  console.log(rows.map(row => scores2[row])
+    .reduce((a, b) => a + b, 0));
+}
+day02();
